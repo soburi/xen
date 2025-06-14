58cca14a9daf387723a43c4ca716809287be72c5
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2025-06-15 08:12:41 +0900

    include: xen: public: xen-compat.h: common defs and interface version
    Add common header including and handling interface version.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- include/xen/public/xen-compat.h
+++ include/xen/public/xen-compat.h
@@ -14,7 +14,13 @@
 
 #if defined(__XEN__) || defined(__XEN_TOOLS__)
 /* Xen is built with matching headers and implements the latest interface. */
+
+/* modification for zephyr: If a version is specified explicitly, it will be used. */
+#if CONFIG_XEN_INTERFACE_VERSION
+#define __XEN_INTERFACE_VERSION__ CONFIG_XEN_INTERFACE_VERSION
+#else
 #define __XEN_INTERFACE_VERSION__ __XEN_LATEST_INTERFACE_VERSION__
+#endif
 #elif !defined(__XEN_INTERFACE_VERSION__)
 /* Guests which do not specify a version get the legacy interface. */
 #define __XEN_INTERFACE_VERSION__ 0x00000000
@@ -25,5 +31,11 @@
 #endif
 
 #define COMPAT_FLEX_ARRAY_DIM XEN_FLEX_ARRAY_DIM
+
+/* modification for zephyr: add common definitions. */
+#if !defined(_ASMLANGUAGE)
+#include <stdbool.h>
+#include <stdint.h>
+#endif
 
 #endif /* __XEN_PUBLIC_XEN_COMPAT_H__ */
